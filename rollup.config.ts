12f499dfd687fb8c19b46f39f46e483fe4073497
Justin DuJardin <justin@dujardinconsulting.com>
2019-01-15 16:22:06 -0800

    remove prop-types from output bundle

--- rollup.config.ts
+++ rollup.config.ts
@@ -16,7 +16,7 @@ export default {
     { file: pkg.module, format: 'es', sourcemap: true }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: ['react', 'react-dom', 'redux'],
+  external: ['react', 'react-dom', 'redux', 'prop-types'],
   watch: {
     include: 'src/**'
   },
